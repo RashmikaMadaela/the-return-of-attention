6260a761c1779fda686a81b2ffc6e8246f1ecc49
Rashmika Madaela <rashmikamadaelaofficial@gmail.com>
2025-10-26 18:08:30 +0530

    fixed some build errors

--- src/app/stage-1/page.tsx
+++ src/app/stage-1/page.tsx
@@ -1,5 +1,10 @@
+import React, { Suspense } from 'react'
 import Stage1Page from '../../components/Stage1Page'
 
 export default function Page() {
-  return <Stage1Page />
+  return (
+    <Suspense fallback={<div /> }>
+      <Stage1Page />
+    </Suspense>
+  )
 }
